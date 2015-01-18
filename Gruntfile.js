3a5c746b4185dfe72e8b82250734c417d0073cb0
Nicole Beaulieu <nbeaulieu@me.com>
2015-01-17 19:37:21 -0800

    Minify css, js, and html.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,23 +6,38 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         mangle: false, // Do not mangle the file (don't change variables, etc.).
-        //banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       my_target: {
         files: {
-          'views/js/main.min.js': ['views/js/main.js']
-        }      //build: {
-      //  src: 'src/<%= pkg.name %>.js',
-      //  dest: 'build/<%= pkg.name %>.min.js'
-      //}
+          'views/js/main.min.js': ['views/js/main.js'],
+        }
+      }
+    },
+    cssmin: {
+      my_target: {
+        files: {
+          'views/css/bootstrap-grid.min.css': ['views/css/bootstrap-grid.css'],
+        }
+      }
+    },
+    htmlmin: {
+      dev: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'views/pizza.min.html': ['views/pizza.html'],
+        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-css');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
+  grunt.registerTask('default', ['uglify', 'cssmin', 'htmlmin']);
 };
